7cea1b17f36416d22396865bcf25430af7c4a2c1
unknown <daffatabianoo@gmail.com>
2024-10-21 11:43:34 +0700

    feat(auth): login implementations

--- src/helper/styles.js
+++ src/helper/styles.js
@@ -2,5 +2,5 @@ export const styles = {
   input:
     'w-full p-4 rounded-lg bg-gray-200 mt-2 placeholder-gray-600 text-gray-600 focus:bg-white focus:outline-none focus:border focus:border-indigo-500',
   button:
-    'w-full p-4 rounded-lg bg-indigo-500 mt-2 placeholder-gray-600 text-white focus:bg-white focus:outline-none focus:border focus:border-indigo-500',
+    'w-full p-4 rounded-lg bg-indigo-500 mt-2 placeholder-gray-600 text-white focus:bg-white focus:outline-none focus:border focus:border-indigo-500 focus:text-indigo-500',
 };
--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,9 +1,78 @@
+import { useState } from 'react';
 import { styles } from '../../helper/styles';
+import useAuth from '../../hooks/auth/useAuth';
 import AuthLayout from './layout';
+import { FaCheck } from 'react-icons/fa';
+import { MdError } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
+  const navigate = useNavigate();
+  const { login } = useAuth();
+  const [notify, setNotify] = useState({
+    isShown: false,
+    message: '',
+    type: '',
+    icon: '',
+  });
   const handleLogin = async (e) => {
-    e.preventDefault();
+    try {
+      e.preventDefault();
+      const body = {
+        email: e.target.email.value,
+        password: e.target.password.value,
+      };
+      const res = await login(body);
+      if (res.status === 200) {
+        setNotify({
+          isShown: true,
+          message: res.data.message,
+          type: 'success',
+          icon: <FaCheck />,
+        });
+        setTimeout(() => {
+          setNotify({
+            isShown: true,
+            message: 'Redirecting...',
+            type: 'success',
+            icon: <FaCheck />,
+          });
+          setTimeout(() => {
+            navigate('/');
+          }, 1000);
+        }, 1000);
+      } else {
+        setNotify({
+          isShown: true,
+          message: res.data.message,
+          type: 'error',
+          icon: <MdError />,
+        });
+      }
+      setTimeout(() => {
+        setNotify({
+          isShown: false,
+          message: '',
+          type: '',
+          icon: '',
+        });
+      }, 2000);
+    } catch (error) {
+      setNotify({
+        isShown: true,
+        message: error.response.data.message,
+        type: 'error',
+        icon: <MdError />,
+      });
+      setTimeout(() => {
+        setNotify({
+          isShown: false,
+          message: '',
+          type: '',
+          icon: '',
+        });
+      }, 2000);
+    }
   };
 
   return (
@@ -14,6 +83,20 @@ export default function Login() {
           Enter your credentials, to access your account
         </span>
       </h1>
+      {notify.isShown && (
+        <div
+          className={`flex gap-2 items-center ${
+            notify.type === 'success' ? 'bg-green-200' : 'bg-red-200'
+          } p-4 rounded-lg mt-4`}>
+          <i
+            className={`${
+              notify.type === 'success' ? 'text-green-600' : 'text-red-600'
+            }`}>
+            {notify.icon}
+          </i>
+          <p>{notify.message}</p>
+        </div>
+      )}
       <form onSubmit={handleLogin} className="flex flex-col gap-2 mt-4">
         <label htmlFor="email">
           Email
@@ -40,7 +123,7 @@ export default function Login() {
       </form>
 
       <p className="text-center mt-4">
-        Don't have an account?{' '}
+        Don&apos;t have an account?{' '}
         <a href="/register" className="text-indigo-500">
           Register
         </a>
