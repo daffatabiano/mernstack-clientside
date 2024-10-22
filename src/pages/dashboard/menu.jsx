39ee5ede754a0f49db96d6be5e5b9de78f18ff20
unknown <daffatabianoo@gmail.com>
2024-10-22 10:56:41 +0700

    feat(dashboard): slicing menu ui and integated api

--- src/pages/dashboard/menu.jsx
+++ src/pages/dashboard/menu.jsx
@@ -2,16 +2,35 @@ import { useState } from 'react';
 import { listsSubmenu } from '../../helper/constants';
 import { styles } from '../../helper/styles';
 import DashboardLayout from './layout';
+import usePost from '../../hooks/usePost';
+import { FaPercent } from 'react-icons/fa';
 
 export default function MenuDashboard() {
   const [shownAdd, setShownAdd] = useState(false);
+  const [products, setProducts] = useState([]);
+  const { createProduct } = usePost();
 
   const handleAdd = async (e) => {
     e.preventDefault();
+    const body = {
+      category: e.target.category.value,
+      image: e.target.image.value || '/images/empty-food.png',
+      foodname: e.target.foodname.value,
+      description: e.target.description.value,
+      price: e.target.price.value,
+    };
+
+    try {
+      const res = await createProduct(body);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
     <>
+      {/* Modals Add Products */}
       <div
         className={`w-full h-full fixed unset-0 z-20 flex justify-center items-center bg-slate-800/50 p-4 ${
           shownAdd ? 'block' : 'hidden'
@@ -69,12 +88,17 @@ export default function MenuDashboard() {
             </label>
             <label htmlFor="discount">
               Discount
-              <input
-                type="number"
-                name="discount"
-                id="discount"
-                className={styles.input}
-              />
+              <div className="relative ">
+                <input
+                  type="number"
+                  name="discount"
+                  id="discount"
+                  className={styles.input}
+                />
+                <div className="absolute right-0 bg-white bottom-0 h-[90%] px-4 rounded-lg flex items-center justify-center">
+                  <FaPercent />
+                </div>
+              </div>
             </label>
             <div className="w-full flex gap-2">
               <button
@@ -90,8 +114,12 @@ export default function MenuDashboard() {
           </form>
         </div>
       </div>
+      {/* End Modals Add Products */}
+
+      {/* Main Menu Layout */}
       <DashboardLayout>
         <div className="w-full h-full p-4">
+          {/* Header */}
           <div className="flex gap-2 items-center px-2">
             <div className="w-[90%] h-12 gap-4 flex overflow-x-auto overflow-y-hidden">
               {listsSubmenu.map((item) => (
@@ -109,8 +137,35 @@ export default function MenuDashboard() {
               </button>
             </div>
           </div>
+          {/* End Header */}
+
+          {/* Content */}
+          <div className="w-full h-full">
+            {products.length === 0 ? (
+              <div className="w-full h-full flex flex-col gap-2 justify-center items-center">
+                <img
+                  src="/images/empty-products.png"
+                  alt=""
+                  className="w-1/2 h-1/2 object-cover"
+                />
+                <p className="text-2xl font-bold ">
+                  No Products{' '}
+                  <span
+                    onClick={() => setShownAdd(true)}
+                    className="text-indigo-500 hover:underline cursor-pointer">
+                    Added.
+                  </span>
+                </p>
+              </div>
+            ) : (
+              <div>Have some Products</div>
+            )}
+          </div>
+
+          {/* End Content */}
         </div>
       </DashboardLayout>
+      {/* End Menu Layout */}
     </>
   );
 }
