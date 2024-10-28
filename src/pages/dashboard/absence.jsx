619e6c39e042c170ae4420d5e57dc324259cb606
unknown <daffatabianoo@gmail.com>
2024-10-28 23:12:15 +0700

    add(assers): add image ches for page begins

---
+++ src/pages/dashboard/absence.jsx
@@ -0,0 +1,9 @@
+import DashboardLayout from './layout';
+
+export default function AbsenceDashboard() {
+  return (
+    <DashboardLayout>
+      <h1>Absence Dashboard</h1>
+    </DashboardLayout>
+  );
+}
