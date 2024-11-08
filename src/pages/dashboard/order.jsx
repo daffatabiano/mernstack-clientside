908bcda22c3cb9d5d7354351bf4ed1bba2747e92
unknown <daffatabianoo@gmail.com>
2024-11-08 16:18:21 +0700

    feat(orders) : try to implementations for a while

--- src/pages/dashboard/order.jsx
+++ src/pages/dashboard/order.jsx
@@ -3,16 +3,36 @@ import DashboardLayout from './layout';
 
 export default function OrderDashboard() {
   const { data } = useFetch('orders');
-  const orders = data.data;
+  const orders = data?.data;
 
   return (
     <DashboardLayout>
-      <div className="w-full h-full p-4 flex flex-col justify-center items-center">
-        <img src="/images/coming-soon.png" alt="" />
-        <h1 className="text-3xl italic font-bold tracking-widest text-amber-400">
-          Under Construction !
-        </h1>
-      </div>
+      {orders?.length === 0 ? (
+        <div className="w-full h-full p-4 flex flex-col justify-center items-center">
+          <img src="/images/coming-soon.png" alt="" />
+          <h1 className="text-3xl italic font-bold tracking-widest text-amber-400">
+            Under Construction !
+          </h1>
+        </div>
+      ) : (
+        <div className="grid grid-cols-4 gap-2 overflow-auto mt-2 px-2">
+          {orders?.map((order) => (
+            <div
+              className="text-wrap bg-white rounded-lg p-4 drop-shadow-sm"
+              key={order._id}>
+              <h1>{order?._id}</h1>
+              <p>
+                {new Intl.NumberFormat('id-ID', {
+                  style: 'currency',
+                  currency: 'IDR',
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 0,
+                }).format(order?.amount)}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
     </DashboardLayout>
   );
 }
