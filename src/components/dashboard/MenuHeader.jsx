623a4e0416e8579e52c755c98560a1938ea27ece
unknown <daffatabianoo@gmail.com>
2024-11-30 11:21:52 +0700

    add(dashboard): menu header c

---
+++ src/components/dashboard/MenuHeader.jsx
@@ -0,0 +1,33 @@
+import { CiSquarePlus } from 'react-icons/ci';
+import { styles } from '../../helper/styles';
+
+export default function MenuHeader(prop) {
+  const { submenu, setShowByCategory, setShownAdd } = prop;
+
+  return (
+    <div className="flex items-center border-2 rounded-lg border-indigo-500">
+      <div className="w-[90%] h-12 gap-4 p-2 scrollbar-header-menu flex overflow-x-auto overflow-y-hidden">
+        {submenu.map((item) => (
+          <h1
+            key={item.name}
+            onClick={() =>
+              setShowByCategory(item.name === 'All' ? '' : item.name)
+            }
+            className={styles.submenu}>
+            {item.name}
+          </h1>
+        ))}
+      </div>
+      <div className="w-[10%] h-12">
+        <button
+          type="button"
+          className="w-full h-full flex justify-center items-center bg-indigo-500 text-white"
+          onClick={() => setShownAdd(true)}>
+          <i className="text-4xl">
+            <CiSquarePlus />
+          </i>
+        </button>
+      </div>
+    </div>
+  );
+}
