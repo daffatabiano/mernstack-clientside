7900c8f745afce5207830e61887432f3c72f4bbe
unknown <daffatabianoo@gmail.com>
2024-12-14 23:16:58 +0700

    feat(styles ):  font styles for title styles font

--- src/helper/styles.js
+++ src/helper/styles.js
@@ -12,4 +12,5 @@ export const styles = {
     'z-20 flex flex-col justify-between w-1/5 p-6 bg-white drop-shadow-lg shadow-slate-800 fixed top-0 bottom-0 h-screen overflow-y-auto left-0 transition-all transform duration-300',
   submenu:
     'text-lg bg-indigo-500 text-white font-semibold p-4 rounded-full flex justify-center items-center hover:bg-indigo-600 cursor-pointer',
+  fontTitle: 'font-bold text-indigo-500 text-2xl',
 };
