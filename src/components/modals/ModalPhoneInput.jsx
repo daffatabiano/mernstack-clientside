8391d24106e0f2d68ac9ccfb5315dad66f0c1356
unknown <daffatabianoo@gmail.com>
2024-12-21 08:06:29 +0700

    feat(modals): modal show Phone Input features in main page

--- src/components/modals/ModalPhoneInput.jsx
+++ src/components/modals/ModalPhoneInput.jsx
@@ -3,31 +3,52 @@ import { styles } from '../../helper/styles';
 import ModalCard from './Card';
 import ModalWrapper from './Wrapper';
 import { useState } from 'react';
+import useAction from '../../hooks/useAction';
 
 export default function ModalPhoneInput() {
   const navigate = useNavigate();
   const [phone, setPhone] = useState('');
+  const [isValid, setIsValid] = useState(false);
+  const [isCode, setIsCode] = useState('');
 
-  const handleNavigate = () => {
-    localStorage.setItem('phone', phone);
-    navigate('/menu');
-  };
+  const { sendOtp, verifyOtp } = useAction();
+
+  const handleSendOtp = () =>
+    sendOtp(phone)
+      .then((res) => {
+        if (res.status === 200) setIsValid(true);
+      })
+      .catch((err) => console.log(err));
 
   return (
     <ModalWrapper>
       <ModalCard>
-        <div className="flex flex-col gap-2">
-          <h1 className={`${styles.fontTitle}`}>Input Your Number Phone</h1>
-          <input
-            onChange={(e) => setPhone(e.target.value)}
-            type="number"
-            placeholder="+62 xxx"
-            className={styles.input}
-          />
-        </div>
-        <button className={styles.button} onClick={handleNavigate}>
-          Submit
-        </button>
+        {isValid ? (
+          <div>
+            <h1>Your phone number is {phone}</h1>
+            <input
+              type="text"
+              onChange={(e) => setIsCode(e.target.value)}
+              value={isCode}
+            />
+          </div>
+        ) : (
+          <>
+            <div className="flex flex-col gap-2">
+              <h1 className={`${styles.fontTitle}`}>Input Your Number Phone</h1>
+              <input
+                onChange={(e) => setPhone(e.target.value)}
+                type="number"
+                placeholder="+62 xxx"
+                className={styles.input}
+                value={phone}
+              />
+            </div>
+            <button className={styles.button} onClick={handleSendOtp}>
+              Send
+            </button>
+          </>
+        )}
       </ModalCard>
     </ModalWrapper>
   );
