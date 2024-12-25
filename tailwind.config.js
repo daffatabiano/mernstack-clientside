208ee9dc951526840fb97e09b6ab5edc14bce55b
unknown <daffatabianoo@gmail.com>
2024-12-25 18:59:40 +0700

    fix config flowbite with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-import flowbite from 'flowbite/tailwind.config.js';
+import flowbite from 'flowbite-react/tailwind';
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', flowbite.content()],
