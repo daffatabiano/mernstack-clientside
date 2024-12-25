8d316a38e1a89a6161045341fe62de0429bf67cd
unknown <daffatabianoo@gmail.com>
2024-12-25 14:38:01 +0700

    feat(modals): add custom class

--- src/components/modals/Card.jsx
+++ src/components/modals/Card.jsx
@@ -1,5 +1,7 @@
-export default function ModalCard({ children }) {
+export default function ModalCard({ children, className }) {
   return (
-    <div className="bg-white rounded-lg w-full md:w-fit p-2">{children}</div>
+    <div className={`bg-white rounded-lg w-full md:w-fit p-2 ${className}`}>
+      {children}
+    </div>
   );
 }
