63d999f1b016fc4ee79740558238f1b9e19135d8
unknown <daffatabianoo@gmail.com>
2025-01-11 14:09:15 +0700

    fix(menu): menu account for changing page

--- src/pages/menu/account.jsx
+++ src/pages/menu/account.jsx
@@ -1,8 +1,14 @@
+import { useState } from 'react';
 import LayoutMenu from './layout';
 
 export default function Account() {
+  const [toCategoryMenu, setToCategoryMenu] = useState({
+    closeCategory: true,
+    menu: {},
+  });
+
   return (
-    <LayoutMenu>
+    <LayoutMenu toCategoryMenu={toCategoryMenu}>
       <div>this is jak mania account</div>
     </LayoutMenu>
   );
