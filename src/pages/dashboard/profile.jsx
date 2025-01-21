0006ae5cfcbfb7af8b0f86bcaf29e5ea64b6c42a
unknown <daffatabianoo@gmail.com>
2025-01-21 19:09:32 +0700

    fix(pages): dashboard profile

--- src/pages/dashboard/profile.jsx
+++ src/pages/dashboard/profile.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react';
-import { styles } from '../../helper/styles';
+import { useRef, useState } from 'react';
 import DashboardLayout from './layout';
-import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { FaCheck } from 'react-icons/fa';
 import { MdErrorOutline } from 'react-icons/md';
+import ModalEditProfile from '../../components/dashboard/modals/modal-edit-profile';
+import ModalCropper from '../../components/dashboard/modals/modal-croper';
+import CardProfile from '../../components/dashboard/card-profile';
+const PLACEHOLDER_IMAGE = import.meta.env.VITE_PLACEHOLDER_IMAGE;
 
 const reusable = {
   styles: {
@@ -17,35 +19,57 @@ export default function ProfileDashboard() {
   const user = JSON.parse(localStorage.getItem('user'));
   const token = localStorage.getItem('token');
   const [showEdit, setShowEdit] = useState(false);
-  const navigate = useNavigate();
   const [updateNotif, setUpdateNotif] = useState({
     isShown: false,
     message: '',
     type: '',
     icon: '',
   });
+  const [image, setImage] = useState('');
+  const [cropData, setCropData] = useState('');
+  const cropperRef = useRef(null);
+  const [showCropModal, setShowCropModal] = useState({
+    shown: false,
+    section: 1,
+  });
 
-  if (!user) return window.location.replace('/login');
+  const onChange = (e) => {
+    e.preventDefault();
+    let files;
+    if (e.dataTransfer) {
+      files = e.dataTransfer.files;
+    } else if (e.target) {
+      files = e.target.files;
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      setImage(reader.result);
+    };
+    reader.readAsDataURL(files[0]);
+  };
+
+  const getCropData = (e) => {
+    e.preventDefault();
+    if (typeof cropperRef.current?.cropper !== 'undefined') {
+      setCropData(cropperRef.current?.cropper.getCroppedCanvas().toDataURL());
+    }
+    setShowCropModal({ ...showCropModal, shown: false });
+  };
 
   const handleEdit = async (e) => {
     e.preventDefault();
     const body = {
+      NIK: e.target.nik.value,
       name: e.target.name.value,
       email: e.target.email.value,
-      password: e.target.password.value,
-      confirmPassword: e.target.confirmPassword.value,
-      age: e.target.age.value,
+      born_date: e.target.age.value,
       gender: e.target.gender.value,
+      image: image
+        ? cropData
+        : `${PLACEHOLDER_IMAGE}/30/dd6699/ffffff/100x100.png?text=${user.name
+            ?.slice(0, 2)
+            .toUpperCase()}`,
     };
-
-    if (body.password !== body.confirmPassword) {
-      return setUpdateNotif({
-        isShown: true,
-        message: 'Passwords do not match',
-        type: 'error',
-        icon: <MdErrorOutline />,
-      });
-    }
 
     if (!body) {
       return setUpdateNotif({
@@ -118,125 +142,27 @@ export default function ProfileDashboard() {
     }
   };
 
-  const ageYear = new Date().getFullYear() - user.born_date?.split('-')[0];
-  const ageMonth = new Date().getMonth() - user.born_date?.split('-')[1];
-  const age = `${ageYear} years, ${ageMonth} months`;
-  const yearsPeriod = new Date().getFullYear() - user.createdAt?.split('-')[0];
-  const monthsPeriod = new Date().getMonth() - user.createdAt?.split('-')[1];
-  const getHours = user.createdAt.split('-')[2].split('T')[1].split(':')[0];
-  const dayHoursPeriod = new Date().getHours() - Number(getHours);
-  const workPeriods = (y, m, h) => {
-    if (yearsPeriod > 0) {
-      return `${y} years ${m} months`;
-    } else if (monthsPeriod > 0) {
-      return `${m} months ${h} hours`;
-    } else {
-      return `${h} hours`;
-    }
-  };
-
   return (
     <>
-      <div
-        className={`bg-slate-800/50 h-screen w-full flex justify-center items-center fixed top-0 right-0 left-0 z-50 ${
-          showEdit ? 'block' : 'hidden'
-        }`}>
-        <div
-          className={`absolute top-5 right-5 bg-white p-4 rounded border-b-8 ${
-            updateNotif.type === 'error' ? 'border-red-500' : 'border-green-500'
-          } ${updateNotif.isShown ? 'block' : 'hidden'}`}>
-          <div
-            className={`flex items-center gap-4 ${
-              updateNotif.type === 'error' ? 'text-red-500' : 'text-green-500'
-            }`}>
-            {updateNotif.icon}
-            <p>{updateNotif.message}</p>
-          </div>
-        </div>
-        <div className="w-1/3 max-h-[90%] overflow-y-auto bg-white rounded-lg p-4 flex flex-col justify-center items-center">
-          <h1 className="text-2xl font-bold pt-12">Edit Profile</h1>
-          <form onSubmit={handleEdit}>
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
-              className="w-32 h-32 my-4 mx-auto rounded-full"
-              alt=""
-            />
+      <ModalEditProfile
+        showEdit={showEdit}
+        updateNotif={updateNotif}
+        handleEdit={handleEdit}
+        setShowCropModal={setShowCropModal}
+        image={image}
+        showCropModal={showCropModal}
+        setShowEdit={setShowEdit}
+      />
 
-            <label htmlFor="name">
-              Name
-              <input
-                type="text"
-                name="name"
-                placeholder="Name"
-                className={styles.input}
-                defaultValue={user.name}
-              />
-            </label>
-            <label htmlFor="password">
-              Password
-              <input
-                type="password"
-                name="password"
-                placeholder="********"
-                className={styles.input}
-              />
-            </label>
-            <label htmlFor="confirmpassword">
-              Password
-              <input
-                type="password"
-                name="confirmPassword"
-                placeholder="********"
-                className={styles.input}
-              />
-            </label>
-            <label htmlFor="email">
-              Email
-              <input
-                type="text"
-                name="email"
-                placeholder="Email"
-                className={styles.input}
-                defaultValue={user.email}
-              />
-            </label>
-            <div className="flex gap-2">
-              <label htmlFor="gender" className="flex flex-col w-full">
-                Gender
-                <select name="gender" id="" className={styles.input}>
-                  <option value="male" selected={user.gender === 'male'}>
-                    Male
-                  </option>
-                  <option value="female" selected={user.gender !== 'male'}>
-                    Female
-                  </option>
-                </select>
-              </label>
-              <label htmlFor="age">
-                Age
-                <input
-                  type="number"
-                  name="age"
-                  placeholder="Age"
-                  className={styles.input}
-                  defaultValue={user.age}
-                />
-              </label>
-            </div>
-            <div className="flex gap-2">
-              <button
-                className={styles.button}
-                type="button"
-                onClick={() => setShowEdit(!showEdit)}>
-                Close
-              </button>
-              <button className={styles.button} type="submit">
-                Update
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+      <ModalCropper
+        setShowCropModal={setShowCropModal}
+        showCropModal={showCropModal}
+        updateNotif={updateNotif}
+        getCropData={getCropData}
+        image={image}
+        onChange={onChange}
+        cropperRef={cropperRef}
+      />
 
       <DashboardLayout>
         <section className="p-4 h-full w-full flex gap-5 items-center">
@@ -248,50 +174,8 @@ export default function ProfileDashboard() {
               </i>{' '}
               Keep Spirit!
             </h1>
-            <div className="p-4 w-full h-[90%] gap-4 rounded flex flex-col items-center bg-white">
-              <h2 className="text-2xl text-indigo-500">Account Information</h2>
 
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
-                alt=""
-                className="w-20 h-20 rounded-full"
-              />
-
-              <div className="flex flex-col gap-4 w-full bg-slate-100 p-4 rounded">
-                <p>
-                  <span className="font-bold text-indigo-500">NIK: </span>
-                  {'1234567890'}
-                </p>
-                <p>
-                  <span className="font-bold text-indigo-500">Email: </span>
-                  {user.email}
-                </p>
-                <p>
-                  <span className="font-bold text-indigo-500">Age: </span>
-                  {age}
-                </p>
-                <p>
-                  <span className="font-bold text-indigo-500">Gender: </span>
-                  {user.gender}
-                </p>
-                <p>
-                  <span className="font-bold text-indigo-500">Role: </span>
-                  {'Cashier'}
-                </p>
-                <p>
-                  <span className="font-bold text-indigo-500">
-                    Works Period:{' '}
-                  </span>
-                  {workPeriods(yearsPeriod, monthsPeriod, dayHoursPeriod)}
-                </p>
-              </div>
-              <button
-                type="button"
-                onClick={() => setShowEdit(true)}
-                className={` ${styles.button}`}>
-                Edit
-              </button>
-            </div>
+            <CardProfile setShowEdit={setShowEdit} />
           </div>
           <div className="flex flex-col gap-4 w-2/3">
             <h1 className="text-2xl font-bold text-end uppercase">
