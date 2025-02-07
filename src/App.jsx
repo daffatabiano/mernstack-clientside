3ad39e1aa6052b0a00877fbd051d05762a7c9f39
unknown <daffatabianoo@gmail.com>
2025-02-07 21:25:41 +0700

    feat(app): give notification in parents APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 import { useRoutes } from 'react-router-dom';
 import { routeLists } from './routes/routeLists';
+import { useDispatch, useSelector } from 'react-redux';
+import { notification } from 'antd';
+import { useEffect } from 'react';
+import { clearNotification } from './redux/reducers/notificationReducers';
+import { loadingAnimation } from './assets';
 
 function App() {
+  const dispatch = useDispatch();
+  const { visible, message, type, description } = useSelector(
+    (state) => state?.notification
+  );
+  const load = useSelector((state) => console.log(state.loading));
+
+  useEffect(() => {
+    if (visible)
+      notification[type]({
+        message,
+        description,
+        onClose: () => dispatch(clearNotification()),
+      });
+  }, [visible]);
+
+  // useEffect(() => {
+  //   if (loading) {
+  //     return (
+  //       <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-slate-800/50 z-[100]">
+  //         <img src={loadingAnimation} alt="loading-animation" />
+  //       </div>
+  //     );
+  //   }
+  // }, [loading]);
+
   const elements = useRoutes(routeLists);
 
   return elements;
