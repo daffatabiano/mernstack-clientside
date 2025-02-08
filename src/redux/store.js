2111f539a7217254a13a85e560b0bfa71f51a6af
unknown <daffatabianoo@gmail.com>
2025-02-08 16:13:26 +0700

    add(redux): store for refactoring API

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducers';
+import { combineMiddleware } from './middleware';
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (GDM) => {
+    return GDM().concat(...combineMiddleware);
+  },
 });
 
 export default store;
