9c1541014fa7864575828ba4579dde3d630642f7
unknown <daffatabianoo@gmail.com>
2025-02-08 19:49:52 +0700

    feat(redux): middleware delete

--- src/redux/middleware.js
+++ src/redux/middleware.js
@@ -1,4 +1,9 @@
+import { deleteMiddleware } from './reducers/api/deleteReducers';
 import { fetchMiddleware } from './reducers/api/fetchReducers';
 import { postMiddleware } from './reducers/api/postReducers';
 
-export const combineMiddleware = [postMiddleware, fetchMiddleware];
+export const combineMiddleware = [
+  postMiddleware,
+  fetchMiddleware,
+  deleteMiddleware,
+];
