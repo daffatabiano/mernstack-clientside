5d3e4a54fa5335714ef377039798658d619db28a
unknown <daffatabianoo@gmail.com>
2025-02-08 19:48:31 +0700

    fix(dashboard): analytics

--- src/pages/dashboard/analytics.jsx
+++ src/pages/dashboard/analytics.jsx
@@ -1,14 +1,10 @@
-import DashboardLayout from './layout';
-
 export default function AnalyticsDashboard() {
   return (
-    <DashboardLayout>
-      <div className="w-full h-full p-4 flex flex-col justify-center items-center">
-        <img src="/images/coming-soon.png" alt="" />
-        <h1 className="text-3xl italic font-bold tracking-widest text-amber-400">
-          Under Construction !
-        </h1>
-      </div>
-    </DashboardLayout>
+    <div className="w-full h-full p-4 flex flex-col justify-center items-center">
+      <img src="/images/coming-soon.png" alt="" />
+      <h1 className="text-3xl italic font-bold tracking-widest text-amber-400">
+        Under Construction !
+      </h1>
+    </div>
   );
 }
