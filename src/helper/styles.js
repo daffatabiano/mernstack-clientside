a7527c7c0999ad56f9cdc2e83741aad09306ccb7
unknown <daffatabianoo@gmail.com>
2025-02-08 16:01:39 +0700

    feat(helper): styles helper features

--- src/helper/styles.js
+++ src/helper/styles.js
@@ -9,8 +9,8 @@ export const styles = {
   formAuth:
     'flex flex-col gap-2 mt-4 overflow-hidden transition-all transform duration-300',
   aside:
-    'z-20 flex flex-col justify-between w-1/5 p-6 bg-white drop-shadow-lg shadow-slate-800 fixed top-0 bottom-0 h-screen overflow-y-auto left-0 transition-all transform duration-300',
+    'z-20 flex flex-col justify-between max-w-1/4 p-6 bg-white drop-shadow-lg shadow-slate-800 fixed top-0 bottom-0 h-screen overflow-y-auto left-0 transition-all transform duration-300',
   submenu:
-    'text-lg bg-indigo-500 text-white font-semibold p-4 rounded-full flex justify-center items-center hover:bg-indigo-600 cursor-pointer',
+    'text-[10px] bg-base-dark text-base-light font-semibold p-1 px-4 rounded-full flex justify-center items-center hover:bg-middle-dark cursor-pointer',
   fontTitle: 'font-bold text-indigo-500 text-xl',
 };
