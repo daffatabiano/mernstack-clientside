3334e3d160e948cbecf80d6348fc1036a5cb9ac3
unknown <daffatabianoo@gmail.com>
2025-02-08 16:05:45 +0700

    feat(auth): login

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,95 +1,21 @@
-import { useEffect, useState } from 'react';
 import { styles } from '../../helper/styles';
-import useAuth from '../../hooks/auth/useAuth';
 import AuthLayout from './layout';
-import { FaCheck } from 'react-icons/fa';
-import { MdError } from 'react-icons/md';
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { Button, Form, Input, Spin } from 'antd';
+import { useLoginAdminAccessMutation } from '../../redux/reducers/api/postReducers';
 
 export default function Login() {
-  const tokenExist = localStorage.getItem('token');
-  const dataRegister = useSelector((state) => state.user);
   const navigate = useNavigate();
-  const { login } = useAuth();
-  const [notify, setNotify] = useState({
-    isShown: false,
-    message: '',
-    type: '',
-    icon: '',
-  });
+  const [loginAdminAccess, { isLoading }] = useLoginAdminAccessMutation();
+  const { Item } = Form;
+  const [form] = Form.useForm();
 
-  useEffect(() => {
-    if (tokenExist) {
-      navigate('/');
-    }
-  }, [tokenExist, navigate]);
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    const body = {
-      email: e.target.email.value,
-      password: e.target.password.value,
-    };
-    try {
-      const res = await login(body);
-      if (res?.status === 200) {
-        setNotify({
-          isShown: true,
-          message:
-            res?.data?.message +
-            ', Welcome Back ' +
-            res?.data?.data?.name +
-            '!',
-          type: 'success',
-          icon: <FaCheck />,
-        });
-        setTimeout(() => {
-          setNotify({
-            isShown: true,
-            message: 'Redirecting...',
-            type: 'success',
-            icon: <FaCheck />,
-          });
-          localStorage.setItem('token', res?.data?.token);
-          setTimeout(() => {
-            navigate('/admin-panel/dashboard');
-            console.log(res);
-            localStorage.setItem('user', JSON.stringify(res?.data?.data));
-          }, 1000);
-        }, 1000);
-      } else {
-        setNotify({
-          isShown: true,
-          message: res?.data?.message,
-          type: 'error',
-          icon: <MdError />,
-        });
-      }
-      setTimeout(() => {
-        setNotify({
-          isShown: false,
-          message: '',
-          type: '',
-          icon: '',
-        });
-      }, 2000);
-    } catch (error) {
-      setNotify({
-        isShown: true,
-        message: error?.response.data.message,
-        type: 'error',
-        icon: <MdError />,
-      });
-      setTimeout(() => {
-        setNotify({
-          isShown: false,
-          message: '',
-          type: '',
-          icon: '',
-        });
-      }, 2000);
-    }
+  const handleLogin = async () => {
+    const values = form.getFieldsValue();
+    await loginAdminAccess({
+      ...values,
+      navigate: navigate,
+    });
   };
 
   return (
@@ -100,51 +26,40 @@ export default function Login() {
           Enter your credentials, to access Admin dashboard
         </span>
       </h1>
-      {notify.isShown && (
-        <div
-          className={`flex gap-2 items-center ${
-            notify.type === 'success' ? 'bg-green-200' : 'bg-red-200'
-          } p-4 rounded-lg mt-4`}>
-          <i
-            className={`${
-              notify.type === 'success' ? 'text-green-600' : 'text-red-600'
-            }`}>
-            {notify.icon}
-          </i>
-          <p>{notify.message}</p>
-        </div>
-      )}
-      <form onSubmit={handleLogin} className={styles.formAuth}>
-        <label htmlFor="email">
-          Email
-          <input
-            type="text"
-            name="email"
-            placeholder="example@ex.com"
-            className={styles.input}
-            defaultValue={dataRegister?.user ? dataRegister?.user?.email : ''}
-          />
-        </label>
-        <label htmlFor="password">
-          Password
-          <input
-            type="password"
+      <Form form={form}>
+        <div className="flex flex-col pt-4">
+          <Item name="email" rules={[{ required: true, type: 'email' }]}>
+            <Input placeholder="example@ex.com" allowClear />
+          </Item>
+          <Item
             name="password"
-            placeholder="********"
-            className={styles.input}
-          />
-        </label>
+            rules={[
+              {
+                required: true,
+                type: 'string',
+                min: 8,
+              },
+            ]}>
+            <Input.Password placeholder="********" allowClear />
+          </Item>
+        </div>
+        <Button
+          disabled={isLoading}
+          className={styles.button}
+          onClick={handleLogin}>
+          {isLoading ? <Spin /> : 'Login'}
+        </Button>
+      </Form>
 
-        <button type="submit" className={styles.button}>
-          Login
-        </button>
-      </form>
-
-      <p className="text-center mt-4">
+      <p className="text-center text-sm mt-4 text-slate-400">
         Don&apos;t have an account?{' '}
-        <a href="/register" className="text-indigo-500">
-          Register
-        </a>
+        {isLoading ? (
+          <span className="text-slate-500 cursor-not-allowed ">Register</span>
+        ) : (
+          <Link to="/register" className="text-indigo-500 font-semibold">
+            Register
+          </Link>
+        )}
       </p>
     </AuthLayout>
   );
