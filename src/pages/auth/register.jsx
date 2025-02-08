5d95c0a87eadf8433189f06974cf873df902e28c
unknown <daffatabianoo@gmail.com>
2025-02-08 16:06:17 +0700

    feat(auth): register auth refactoring with redux

--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -1,72 +1,28 @@
 import { useState } from 'react';
 import { styles } from '../../helper/styles';
 import AuthLayout from './layout';
-import { MdError } from 'react-icons/md';
-import useAuth from '../../hooks/auth/useAuth';
-import { useNavigate } from 'react-router-dom';
-import { FaCheck } from 'react-icons/fa';
-import { useDispatch } from 'react-redux';
-import { login } from '../../redux/reducers/userReducers';
+import { Link } from 'react-router-dom';
+import { Button, DatePicker, Form, Input, Select, Spin } from 'antd';
+import { useRegisterAdminAccessMutation } from '../../redux/reducers/api/postReducers';
 
 export default function Register() {
-  const dispatch = useDispatch();
-  const { register } = useAuth();
-  const navigate = useNavigate();
-  const [notify, setNotify] = useState({
-    isShown: false,
-    message: '',
-    type: '',
-    icon: '',
-  });
+  const [form] = Form.useForm();
+  const { Item } = Form;
+  const [registerAdminAccess, { isLoading }] = useRegisterAdminAccessMutation();
+  const [bornDate, setBornDate] = useState('');
+
+  const onChangeDate = (date, dateString) => {
+    setBornDate(dateString);
+  };
+
   const [sections, setSections] = useState(1);
-  const handleRegister = async (e) => {
+  const handleRegister = async () => {
+    const values = form.getFieldsValue();
     const body = {
-      name: e.target.name.value,
-      email: e.target.email.value,
-      password: e.target.password.value,
-      confirmPassword: e.target.confirmPassword.value,
-      born_date: e.target.age.value,
-      gender: e.target.gender.value,
+      ...values,
+      born_date: bornDate,
     };
-    e.preventDefault();
-    try {
-      const res = await register(body);
-      if (res.status === 200) {
-        dispatch(login(res.data.data));
-        setNotify({
-          isShown: true,
-          message: res.data.message,
-          type: 'success',
-          icon: <FaCheck />,
-        });
-        setTimeout(() => {
-          setNotify({
-            isShown: true,
-            message: 'Redirecting...',
-            type: 'success',
-            icon: <FaCheck />,
-          });
-          setTimeout(() => {
-            navigate('/login');
-          }, 1000);
-        }, 1000);
-      }
-    } catch (err) {
-      setNotify({
-        isShown: true,
-        message: err.response.data.message,
-        type: 'error',
-        icon: <MdError />,
-      });
-      setTimeout(() => {
-        setNotify({
-          isShown: false,
-          message: '',
-          type: '',
-          icon: '',
-        });
-      }, 3000);
-    }
+    await registerAdminAccess(body);
   };
 
   return (
@@ -78,131 +34,127 @@ export default function Register() {
         </span>
       </h1>
 
-      {notify.isShown && (
-        <div
-          className={`flex p-4 mb-4 text-sm gap-2 items-center ${
-            notify.type === 'error'
-              ? 'bg-red-100 text-red-700'
-              : 'bg-green-100 text-green-700'
-          } border border-${
-            notify.type === 'error' ? 'red' : 'green'
-          }-500 rounded-lg text-${notify.type}-700`}
-          role="alert">
-          {notify.icon}
-          <span className="sr-only">Info</span>
-          <div>
-            <span className="font-medium">{notify.message}</span>
-          </div>
-        </div>
-      )}
-
-      <form onSubmit={handleRegister} className={styles.formAuth}>
+      <Form
+        style={{
+          overflow: 'hidden',
+          display: 'flex',
+          alignItems: 'flex-end',
+          height: sections !== 1 ? '280px' : 'auto',
+        }}
+        form={form}>
         <section
-          className={`flex flex-col gap-2  ${
-            sections !== 1 ? 'translate-x-[100%] hidden' : 'translate-x-[0%]'
+          className={`flex flex-col pt-4 transition-all transform duration-300 ${
+            sections !== 1
+              ? 'translate-x-[103%] opacity-0 w-0'
+              : 'translate-x-[0%] w-full'
           }`}>
-          <label htmlFor="">
-            Name
-            <input
-              type="text"
-              placeholder="John Doe"
-              className={styles.input}
-              name="name"
-              required
-            />
-          </label>
-          <label htmlFor="">
-            Email
-            <input
-              type="email"
-              placeholder="8Xs8s@example.com"
-              className={styles.input}
-              name="email"
-              required
-            />
-          </label>
-          <label htmlFor="">
-            Password
-            <input
-              type="password"
-              placeholder="*******"
-              className={styles.input}
-              name="password"
-              required
-            />
-          </label>
-          <label htmlFor="">
-            Confirm Password
-            <input
-              type="password"
-              placeholder="*******"
-              className={styles.input}
-              name="confirmPassword"
-              required
-            />
-          </label>
+          <label htmlFor="name">Name</label>
+          <Item
+            name="name"
+            rules={[{ required: true, message: 'Name field is required' }]}>
+            <Input allowClear type="text" placeholder="John Doe" />
+          </Item>
+          <label htmlFor="name">Email</label>
+          <Item
+            name="email"
+            rules={[{ required: true, message: 'Email field is required' }]}>
+            <Input allowClear placeholder="8Xs8s@example.com" />
+          </Item>
+          <label htmlFor="name">Password</label>
+          <Item
+            name="password"
+            rules={[{ required: true, message: 'Password field is required' }]}>
+            <Input.Password placeholder="********" />
+          </Item>
+          <label htmlFor="name">Retype Password</label>
+          <Item
+            name="confirmPassword"
+            rules={[
+              { required: true, message: 'Confirm Password field is required' },
+            ]}>
+            <Input.Password placeholder="********" />
+          </Item>
+          <Button
+            htmlType="button"
+            className={styles.button}
+            onClick={() => setSections(sections + 1)}>
+            Next
+          </Button>
         </section>
         <section
-          className={`flex flex-col gap-2 transition-all transform duration-300 ${
-            sections !== 1 ? 'translate-x-[0%]' : 'translate-x-[100%] hidden'
+          className={`flex flex-col mt-2 h-auto gap-2 transition-all transform duration-300 ${
+            sections === 1
+              ? 'translate-x-[103%] opacity-0 w-0'
+              : 'translate-x-[0%] w-full'
           }`}>
-          <label htmlFor="">
-            Born Date
-            <input
-              type="date"
-              min="1"
-              max="99"
-              step="1"
-              pattern="\d{4}-\d{2}-\d{2}"
-              name="age"
-              placeholder="Age"
-              className={styles.input}
-              required
+          <label htmlFor="NIK">NIK</label>
+          <Item
+            name="NIK"
+            style={{ width: '100%', margin: '0' }}
+            rules={[
+              {
+                required: true,
+                message: 'NIK must be 16 characters',
+                length: 16,
+              },
+            ]}>
+            <Input allowClear type="text" placeholder="Input you NIK" />
+          </Item>
+          <label htmlFor="age">Born Date</label>
+          <Item
+            style={{ width: '100%', margin: '0' }}
+            rules={[{ required: true, message: 'Age field is required' }]}>
+            <DatePicker style={{ width: '100%' }} onChange={onChangeDate} />
+          </Item>
+          <label htmlFor="gender">Gender</label>
+          <Item
+            style={{ width: '100%', margin: '0' }}
+            name="gender"
+            rules={[{ required: true, message: 'Gender field is required' }]}>
+            <Select
+              name="gender"
+              style={{ width: '100%' }}
+              placeholder="Select Gender"
+              options={[
+                { value: 'Male', label: 'Male' },
+                { value: 'Female', label: 'Female' },
+              ]}
             />
-          </label>
-          <label htmlFor="">
-            Gender
-            <select name="gender" className={styles.input} required>
-              <option value="">Select</option>
-              <option value="Male">Male</option>
-              <option value="Female">Female</option>
-            </select>
-          </label>
+          </Item>
           <div className="flex justify-between gap-2">
-            <button
-              type="button"
+            <Button
+              htmlType="button"
               className={`${styles.button} ${
                 sections === 1 ? 'translate-x-[100%]' : 'translate-x-[0%] '
               }`}
+              disabled={isLoading}
               onClick={() => setSections(1)}>
               Back
-            </button>
-            <button
-              type="submit"
-              className={`${styles.button} ${
+            </Button>
+            <Button
+              disabled={isLoading}
+              htmlType="button"
+              onClick={handleRegister}
+              className={`${styles.button}  ${
                 sections === 1 ? 'translate-x-[100%]' : 'translate-x-[0%]'
               }`}>
-              Sign Up
-            </button>
+              {isLoading ? <Spin /> : 'Register'}
+            </Button>
           </div>
         </section>
-        <button
-          type="button"
-          onClick={() => setSections(sections + 1)}
-          className={` ${
-            sections === 1 ? 'translate-x-[0%]' : 'translate-x-[100%] hidden'
-          } ${styles.button}`}>
-          Next
-        </button>
-        <p className="text-sm text-gray-400 mt-4 text-center">
-          Already have an account?{' '}
-          <a
-            href="/login"
+      </Form>
+      <p className="text-sm text-gray-400 mt-4 text-center">
+        Already have an account?{' '}
+        {isLoading ? (
+          <span className="text-slate-500 cursor-not-allowed">Sign In</span>
+        ) : (
+          <Link
+            to="/login"
             className="text-indigo-500 hover:text-indigo-600 font-semibold">
             Sign In
-          </a>
-        </p>
-      </form>
+          </Link>
+        )}
+      </p>
     </AuthLayout>
   );
 }
