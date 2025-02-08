028e2cbef79c40836711d417c64a8a447bf96f35
unknown <daffatabianoo@gmail.com>
2025-02-08 15:58:55 +0700

    add(dashboard): add dashboard modal delete components

--- src/components/dashboard/modals/modal-delete.jsx
+++ src/components/dashboard/modals/modal-delete.jsx
@@ -1,30 +1,58 @@
+import { useState } from 'react';
+import useDelete from '../../../hooks/useDelete';
 import { Toaster } from '../../notif/Toaster';
+import useFetch from '../../../hooks/useGet';
 
 export default function ModalDelete(prop) {
-  const {
-    showDelete,
-    setShowDelete,
-    deleteProductHandler,
-    showToast,
-    setShowToast,
-    isNotify,
-  } = prop;
+  const { showDelete, setShowDelete } = prop;
+  const { deleteProduct } = useDelete();
+  const { loading, refetch } = useFetch('products');
+  const [notify, setNotify] = useState({
+    type: '',
+    message: '',
+  });
+  const [showToast, setShowToast] = useState(false);
+
+  const deleteProductHandler = async (id) => {
+    try {
+      const res = await deleteProduct(id);
+      if (res.status === 200) {
+        setShowToast(true);
+        setNotify({
+          message: res.data.message,
+          type: 'success',
+        });
+        setShowDelete({
+          isShown: false,
+        });
+        await refetch();
+      } else {
+        setShowToast(true);
+        setNotify({
+          message: res.data.message,
+          type: 'error',
+        });
+      }
+    } catch (err) {
+      setShowToast(true);
+      setNotify({
+        message: err?.response?.data?.message || err?.message || err,
+        type: 'error',
+      });
+    }
+  };
 
   return (
     <div
-      className={`w-full h-full fixed unset-0 z-20 flex justify-center items-center bg-slate-800/50 p-4 ${
+      className={`w-full h-full fixed unset-0 z-[100] flex justify-center items-center bg-slate-800/50 p-4 ${
         showDelete.isShown ? 'block' : 'hidden'
       }`}>
-      <Toaster
-        showToast={showToast}
-        setShowToast={setShowToast}
-        isStatus={isNotify}
-      />
       <div className="p-4 flex flex-col gap-2 bg-white rounded-lg overflow-auto">
         <h1>Are you sure you want to delete this product?</h1>
         <div className="flex gap-2 justify-end">
           <button
             type="button"
+            disabled={loading}
             onClick={() => setShowDelete({ isShown: false })}
             className="px-4 py-2 bg-indigo-500 text-white rounded">
             Cancel
@@ -32,6 +60,7 @@ export default function ModalDelete(prop) {
           <button
             onClick={() => deleteProductHandler(showDelete.id)}
             type="button"
+            disabled={loading}
             className="px-4 py-2 bg-red-500 text-white rounded">
             Delete
           </button>
