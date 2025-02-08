322012b99c32d16a0ddd8aac3df5f22bcef0e5cb
unknown <daffatabianoo@gmail.com>
2025-02-08 16:02:36 +0700

    add(hooks): use post featues

--- src/hooks/usePost.js
+++ src/hooks/usePost.js
@@ -1,35 +1,55 @@
 import axios from 'axios';
+import { useState } from 'react';
 
 const usePost = () => {
   const url = import.meta.env.VITE_API_URL;
+  const token = localStorage.getItem('token');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const createProduct = async (body) => {
-    const res = await axios.post(`${url}/product`, body, {
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        Authorization: `${localStorage.getItem('token')}`,
-      },
-    });
-
-    return res;
+    try {
+      setLoading(true);
+      const res = await axios.post(`${url}/product`, body, {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: token,
+        },
+      });
+      setError(null);
+      return res;
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
   };
 
   const updateProduct = async (id, body) => {
-    const res = await axios.put(`${url}/product/${id}`, body, {
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        Authorization: `${localStorage.getItem('token')}`,
-      },
-    });
-
-    return res;
+    try {
+      setLoading(true);
+      const res = await axios.put(`${url}/product/${id}`, body, {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: token,
+        },
+      });
+      setError(null);
+      return res;
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return {
     createProduct,
     updateProduct,
+    loading,
+    error,
   };
 };
 
