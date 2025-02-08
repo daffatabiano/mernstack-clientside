699c9ffe7449541817378e2cade00c8fa0082121
unknown <daffatabianoo@gmail.com>
2025-02-08 16:11:51 +0700

    feat(redux): reducers features

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,10 +2,18 @@ import { combineReducers } from 'redux';
 
 import userReducer from './userReducers';
 import cartReducers from './cartReducers';
+import notificationReducers from './notificationReducers';
+import loadingReducers from './loadingReducers';
+import postReducers from './api/postReducers';
+import fetchReducers from './api/fetchReducers';
 
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducers,
+  notification: notificationReducers,
+  loading: loadingReducers,
+  [postReducers.reducerPath]: postReducers.reducer,
+  [fetchReducers.reducerPath]: fetchReducers.reducer,
 });
 
 export default rootReducer;
