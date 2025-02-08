f74635d54f075ab46887d78def666e0ce10d4ac4
unknown <daffatabianoo@gmail.com>
2025-02-08 16:07:33 +0700

    add(dashboard): index dashboard

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -45,60 +45,56 @@ export default function Dashboard() {
   };
 
   return (
-    <DashboardLayout>
-      <div className="w-full h-full p-4 flex flex-col justify-center items-center">
-        <div className="overflow-x-auto">
-          <h1 className="text-3xl font-bold text-indigo-500">Table</h1>
-          <p>
-            {isChecked.length > 0 && `${isChecked.length} of ${data.length}`}
-          </p>
-          <table className="table-auto border-collapse border border-gray-300 w-full text-sm text-left text-gray-700">
-            <thead className="bg-gray-100 border-b border-gray-300">
-              <tr>
-                <th className="p-3 text-center">
+    <div className="w-full h-full p-4 flex flex-col justify-center items-center">
+      <div className="overflow-x-auto">
+        <h1 className="text-3xl font-bold text-indigo-500">Table</h1>
+        <p>{isChecked.length > 0 && `${isChecked.length} of ${data.length}`}</p>
+        <table className="table-auto border-collapse border border-gray-300 w-full text-sm text-left text-gray-700">
+          <thead className="bg-gray-100 border-b border-gray-300">
+            <tr>
+              <th className="p-3 text-center">
+                <div className="flex items-center justify-center">
+                  <span className="mr-2">Select All</span>
+                  <input
+                    type="checkbox"
+                    className="form-checkbox"
+                    onChange={handleChangeAll}
+                    checked={isCheckAll}
+                  />
+                </div>
+              </th>
+              <th className="p-3">First Name</th>
+              <th className="p-3">Last Name</th>
+              <th className="p-3">Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr
+                key={item.id}
+                className="hover:bg-gray-50 border-b border-gray-300">
+                <td className="p-3 text-center">
                   <div className="flex items-center justify-center">
-                    <span className="mr-2">Select All</span>
                     <input
                       type="checkbox"
                       className="form-checkbox"
-                      onChange={handleChangeAll}
-                      checked={isCheckAll}
+                      onChange={() => handleChangeCheckbox(item.id)}
+                      checked={isChecked.includes(item.id)}
                     />
                   </div>
-                </th>
-                <th className="p-3">First Name</th>
-                <th className="p-3">Last Name</th>
-                <th className="p-3">Username</th>
+                </td>
+                <td className="p-3">{item.firstName}</td>
+                <td className="p-3">{item.lastName}</td>
+                <td className="p-3">{item.username}</td>
               </tr>
-            </thead>
-            <tbody>
-              {data.map((item) => (
-                <tr
-                  key={item.id}
-                  className="hover:bg-gray-50 border-b border-gray-300">
-                  <td className="p-3 text-center">
-                    <div className="flex items-center justify-center">
-                      <input
-                        type="checkbox"
-                        className="form-checkbox"
-                        onChange={() => handleChangeCheckbox(item.id)}
-                        checked={isChecked.includes(item.id)}
-                      />
-                    </div>
-                  </td>
-                  <td className="p-3">{item.firstName}</td>
-                  <td className="p-3">{item.lastName}</td>
-                  <td className="p-3">{item.username}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        {/* <img src="/images/coming-soon.png" alt="" />
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {/* <img src="/images/coming-soon.png" alt="" />
         <h1 className="text-3xl italic font-bold tracking-widest text-amber-400">
           Under Construction !
         </h1> */}
-      </div>
-    </DashboardLayout>
+    </div>
   );
 }
