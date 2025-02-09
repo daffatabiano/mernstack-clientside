87f139111ca3da467e693569ca9cc15c98c27a85
unknown <daffatabianoo@gmail.com>
2025-02-09 12:46:16 +0700

    feat(redux): add put middle ware

--- src/redux/middleware.js
+++ src/redux/middleware.js
@@ -1,9 +1,11 @@
 import { deleteMiddleware } from './reducers/api/deleteReducers';
 import { fetchMiddleware } from './reducers/api/fetchReducers';
 import { postMiddleware } from './reducers/api/postReducers';
+import { putMiddleware } from './reducers/api/putReducers';
 
 export const combineMiddleware = [
   postMiddleware,
   fetchMiddleware,
   deleteMiddleware,
+  putMiddleware,
 ];
