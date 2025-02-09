44c9b0dd06319acb5ce0fa43b861ec10bfbaf114
unknown <daffatabianoo@gmail.com>
2025-02-09 12:46:46 +0700

    add(redux): api putreducers

--- src/redux/reducers/api/putReducers.js
+++ src/redux/reducers/api/putReducers.js
@@ -6,8 +6,32 @@ const putReducers = createApi({
   reducerPath: 'put-api',
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
+    prepareHeaders: (headers) => {
+      const token = localStorage.getItem('token');
+      if (token) {
+        headers.set('authorization', token);
+      }
+      headers.set('Content-Type', 'application/json');
+      return headers;
+    },
   }),
   endpoints: (builder) => ({
+    updateCategory: builder.mutation({
+      query: (b, id) => ({
+        url: `/category/${id}`,
+        method: 'PUT',
+        body: b,
+      }),
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+        await handlePutLifecycle({
+          dispatch,
+          queryFulfilled,
+          successTitle: 'Update Success',
+          errorTitle: 'Update Failed',
+        });
+        if (arg.callback) arg.callback();
+      },
+    }),
     updateCustomerProfile: builder.mutation({
       query: (b) => ({
         url: `/update-customer/${b.id}`,
@@ -15,24 +39,23 @@ const putReducers = createApi({
         body: b,
       }),
       async onQueryStarted(arg, { dispatch, queryFulfilled }) {
-        const res = await handlePutLifecycle({
+        await handlePutLifecycle({
           dispatch,
           queryFulfilled,
           successTitle: 'Update Success',
           errorTitle: 'Update Failed',
         });
         if (arg.callback) arg.callback();
-        return res;
       },
     }),
     collectOrdersByAdmin: builder.mutation({
       query: (b, id) => ({
-        url: `collect-orders/${id}`,
+        url: `/collect-orders/${id}`,
         method: 'PUT',
         body: b,
       }),
       async onQueryStarted(arg, { dispatch, queryFulfilled }) {
-        const res = await handlePutLifecycle({
+        await handlePutLifecycle({
           dispatch,
           queryFulfilled,
           successTitle: 'Collected',
@@ -40,13 +63,84 @@ const putReducers = createApi({
         });
 
         if (arg.callback) arg.callback();
-        return res;
+      },
+    }),
+    updateProduct: builder.mutation({
+      query: (b, id) => ({
+        url: `/product/${id}`,
+        method: 'PUT',
+        body: b,
+      }),
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+        await handlePutLifecycle({
+          dispatch,
+          queryFulfilled,
+          successTitle: 'Update Success',
+          errorTitle: 'Update Failed',
+        });
+        if (arg.callback) arg.callback();
+      },
+    }),
+    updateAdminProfile: builder.mutation({
+      query: (b, id) => ({
+        url: `/user/${id}`,
+        method: 'PUT',
+        body: b,
+      }),
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+        await handlePutLifecycle({
+          dispatch,
+          queryFulfilled,
+          successTitle: 'Update Success',
+          errorTitle: 'Update Failed',
+        });
+        if (arg.callback) arg.callback();
+      },
+    }),
+    updatePasswordAdmin: builder.mutation({
+      query: (b, id) => ({
+        url: `/user-change-password/${id}`,
+        method: 'PUT',
+        body: b,
+      }),
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+        await handlePutLifecycle({
+          dispatch,
+          queryFulfilled,
+          successTitle: 'Update Success',
+          errorTitle: 'Update Failed',
+        });
+        if (arg.callback) arg.callback();
+      },
+    }),
+    updateVoucher: builder.mutation({
+      query: (b, id) => ({
+        url: `/voucher/${id}`,
+        method: 'PUT',
+        body: b,
+      }),
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+        await handlePutLifecycle({
+          dispatch,
+          queryFulfilled,
+          successTitle: 'Update Success',
+          errorTitle: 'Update Failed',
+        });
+        if (arg.callback) arg.callback();
       },
     }),
   }),
 });
 
-export const { useUpdateCustomerProfileMutation } = putReducers;
+export const {
+  useUpdateCategoryMutation,
+  useUpdateCustomerProfileMutation,
+  useCollectOrdersByAdminMutation,
+  useUpdateProductMutation,
+  useUpdateAdminProfileMutation,
+  useUpdatePasswordAdminMutation,
+  useUpdateVoucherMutation,
+} = putReducers;
 export const putMiddleware = putReducers.middleware;
 
 export default putReducers;
