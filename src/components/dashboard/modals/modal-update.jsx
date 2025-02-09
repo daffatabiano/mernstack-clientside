5b727c4c9f9170bdedc7fd58e3c4e1b2220a0096
unknown <daffatabianoo@gmail.com>
2025-02-09 12:45:19 +0700

    add(dashboard): modal update fix

--- src/components/dashboard/modals/modal-update.jsx
+++ src/components/dashboard/modals/modal-update.jsx
@@ -1,73 +1,69 @@
 import { RiArrowGoBackFill } from 'react-icons/ri';
-import { listsSubmenu } from '../../../helper/constants';
 import { styles } from '../../../helper/styles';
 import { FaCheckCircle, FaPercent } from 'react-icons/fa';
-import { Toaster } from '../../notif/Toaster';
 import Switch from '../../Switch';
-
+import { Modal, Select } from 'antd';
+import {
+  useGetAllProductsQuery,
+  useGetCategoriesQuery,
+} from '../../../redux/reducers/api/fetchReducers';
+import { useUpdateProductMutation } from '../../../redux/reducers/api/putReducers';
 export default function ModalUpdate(prop) {
   const {
     setShowEdit,
     showEdit,
-    handleEdit,
+    // handleEdit,
     product,
     setStatus,
-    isNotify,
-    setShowToast,
-    showToast,
     shownInputPicture,
     setShownInputPicture,
   } = prop;
+  const { data: category } = useGetCategoriesQuery();
+  const [updateProduct, { isLoading, isSuccess }] = useUpdateProductMutation();
+  const { refetch } = useGetAllProductsQuery({
+    refetchOnMountOrArgChange: true,
+  });
+
+  const handleEdit = async (e) => {
+    e.preventDefault();
+    const body = {
+      category: e?.target?.category?.value,
+      image: e?.target?.image?.value,
+      name: e?.target?.name?.value,
+      discount: e?.target?.discount?.value,
+      price: e?.target?.price?.value,
+      status: status,
+    };
+
+    try {
+      await updateProduct(body, showEdit.id).unwrap();
+      if (isSuccess) {
+        setShowEdit({ isShown: false });
+        await refetch();
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
-    <div
-      className={`w-full h-full fixed unset-0 z-20 flex justify-center items-center bg-slate-800/50 p-4 ${
-        showEdit.isShown ? 'block' : 'hidden'
-      }`}>
-      <Toaster
-        showToast={showToast}
-        setShowToast={setShowToast}
-        isStatus={isNotify}
-      />
+    <Modal
+      open={showEdit.isShown}
+      onCancel={() => setShowEdit({ isShown: false })}>
       <div className="p-4 flex flex-col gap-2 min-w-1/3 h-[95%] bg-white rounded-lg overflow-y-auto overflow-x-hidden">
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold text-indigo-500">Edit Product</h1>
           <Switch product={product} setStatus={setStatus} />
-          {/* <label className="inline-flex items-center cursor-pointer">
-            <span className="me-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-              Off
-            </span>
-            <input
-              type="checkbox"
-              className="sr-only peer"
-              onChange={(e) => setStatus(e.target.checked)}
-              defaultChecked={product?.data?.status}
-            />
-            <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-            <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-              On
-            </span>
-          </label> */}
         </div>
         <form onSubmit={handleEdit} className="flex flex-col gap-4">
           <label htmlFor="Name">
             Category
-            <select
+            <Select
               name="category"
               id="category"
-              className={styles.input}
-              defaultValue={product?.data?.category}>
-              {listsSubmenu.map((item) => {
-                return (
-                  <option
-                    key={item.name}
-                    value={item.name}
-                    selected={product?.data?.category === item.name}>
-                    {item.name}
-                  </option>
-                );
-              })}
-            </select>
+              options={category?.data}
+              defaultValue={product?.data?.category}
+            />
           </label>
           <label htmlFor="image">
             Image
@@ -197,6 +193,6 @@ export default function ModalUpdate(prop) {
           </div>
         </form>
       </div>
-    </div>
+    </Modal>
   );
 }
