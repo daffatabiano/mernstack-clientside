57a1ce028ccf7df70c11733ce43a94193bcf5a64
unknown <daffatabianoo@gmail.com>
2025-02-10 20:46:47 +0700

    fix(pages): success-payment

--- src/pages/success-payment.jsx
+++ src/pages/success-payment.jsx
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Button, Result } from 'antd';
 import { useNavigate } from 'react-router-dom';
@@ -14,56 +13,58 @@ export default function SuccesPayment() {
     if (dataPayment) {
       async function sendToOrder() {
         const orderData = JSON.parse(localStorage.getItem('cart'));
-        const token = localStorage.getItem('token');
-        await sendOrderCustomer(orderData).unwrap();
-        if (!isLoading && isSuccess) {
-          localStorage.removeItem('cart');
-          localStorage.removeItem('dataPayment');
-          const count = setInterval(() => {
-            setSuccessIndicator((prevTime) => prevTime - 1);
-          }, 1000);
-          setTimeout(() => {
-            clearInterval(count);
-            navigate('/order');
-          }, 5000);
+        try {
+          await sendOrderCustomer(orderData).unwrap();
+          if (!isLoading && isSuccess) {
+            localStorage.removeItem('cart');
+            localStorage.removeItem('dataPayment');
+          }
+        } catch (error) {
+          console.log(error);
         }
       }
       localStorage.removeItem('dataPayment');
       sendToOrder();
     }
   }, [dataPayment]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSuccessIndicator((prev) => prev - 1);
+    }, 1000);
+
+    if (successIndicator === 0) {
+      navigate('/order');
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [navigate, successIndicator, isSuccess]);
   return (
     <>
-      {isSuccess && (
-        <>
-          <Result
-            status="success"
-            title="Order Has Been Placed Successfully"
-            subTitle="Your order has been placed successfully. Please check your email for more details."
-            extra={[
-              <Button
-                type="primary"
-                key="console"
-                onClick={() => navigate('/')}>
-                Home
-              </Button>,
-              <Button key="buy" onClick={() => navigate('/order')}>
-                See Orders
-              </Button>,
-            ]}
-          />
-          <p className="text-center text-base-dark font-light">
-            in{' '}
-            <span className="text-indigo-500 font-bold">
-              {successIndicator}
-            </span>{' '}
-            seconds
-          </p>
-          <p className="text-center text-middle-dark font-light italic text-sm">
-            You will be redirected to the order page
-          </p>
-        </>
-      )}
+      <div className="flex flex-col place-content-center h-screen">
+        <Result
+          status="success"
+          title="Order Has Been Placed Successfully"
+          subTitle="Your order has been placed successfully. Please check your email for more details."
+          extra={[
+            <Button type="primary" key="console" onClick={() => navigate('/')}>
+              Home
+            </Button>,
+            <Button key="buy" onClick={() => navigate('/order')}>
+              See Orders
+            </Button>,
+          ]}
+        />
+        <p className="text-center text-middle-dark font-light italic text-sm">
+          You will be redirected to the order page
+        </p>
+        <p className="text-center text-base-dark font-light">
+          in{' '}
+          <span className="text-indigo-500 font-bold">{successIndicator}</span>{' '}
+          seconds
+        </p>
+      </div>
     </>
   );
 }
